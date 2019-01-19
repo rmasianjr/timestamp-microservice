7b8c49d11f2b383e36dad366a77a3b2a49e0df89
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-19 22:51:35 +0800

    Add cors dependency

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
